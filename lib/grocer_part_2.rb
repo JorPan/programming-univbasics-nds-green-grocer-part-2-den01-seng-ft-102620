8ea678f52b73f92cb9ff104d33e56fe588dc37e1
Jordan Panasewicz <jbpanasewicz@gmail.com>
2020-10-22 01:28:58 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -6,6 +6,21 @@ def apply_coupons(cart, coupons)
     cart_item = find_item_by_name_in_collection(coupons[index][:item], cart)
     couponed_item_name ="#{coupons[counter][:item]} W/ COUPON"
     cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
+    if cart_item && cart_item[:count] >= coupons[index][:num]
+      if cart_item_with_coupon
+        cart_item_with_coupon[:count] += coupons[index][:num]
+        cart_item[:count] -= coupons[index][:num]
+      else
+        cart_item_with_coupon = {
+          :item => couponed_item_name,
+          :price => coupons[index][:cost] / coupons[index][:num],
+          :count => coupons[index][:num],
+          :clearance => cart_item[:clearance]
+        }
+        cart << cart_item_with_coupon
+        cart_item[:count] -= coupons[index][:num]
+      end 
+    end 
   index += 1 
   end
 end
